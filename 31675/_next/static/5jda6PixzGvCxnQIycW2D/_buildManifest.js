6f97c5c0ddc7048b847eacec1e0f2ee300e7e8ae
Humayd Khan <me@dyamuh.dev>
2026-03-01 10:22:07 -0600

    testign

--- 31675/_next/static/5jda6PixzGvCxnQIycW2D/_buildManifest.js
+++ 31675/_next/static/5jda6PixzGvCxnQIycW2D/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/31675/_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
