7465f5dd6af59f73a29e7cfaaeafbf4fa346f7c4
Humayd Khan <me@dyamuh.dev>
2026-03-01 10:24:28 -0600

    sdfsdf

--- 31675/_next/static/rJI3jv2f5podiP7HNGNAy/_buildManifest.js
+++ 31675/_next/static/rJI3jv2f5podiP7HNGNAy/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/31675/_next/:path+",
-        "destination": "/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
